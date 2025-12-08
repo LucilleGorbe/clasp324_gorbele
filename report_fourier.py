b53a1b2d00199b9d343329b61c593984cbd2e87b
Lucille Gorbe <gorbele@umich.edu>
2025-12-08 14:48:42 -0500

    Initial commit

---
+++ report_fourier.py
@@ -0,0 +1,16 @@
+'''
+This code takes input windspeedRH data and will run an awesome fourier
+transform on it. :D
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-v0_8")
+
+def data_import(fname)
+    '''
+    Docstring for data_import
+    
+    fname: str
+        Input filename to generate data from
+    '''
